12189e81d37f02c1551d050aa064f4c8ee5560ee
Benjamin W Stookey <ben.stookey@gmail.com>
2018-09-06 15:32:27 +0000

    another refactor to include base index.js file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export { default as support } from "./reducers";
+export {
+    supportItemRequest,
+    supportItemSuccess,
+    supportItemFailure,
+    closeSupport
+} from "./actions";
+export { default as SupportIcon } from "./components/SupportIcon";
+export { default as SupportDrawer } from "./containers/SupportDrawer";
